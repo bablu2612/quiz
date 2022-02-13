f0f19c3201459b8542bcc1b0f978f332a41082c9
Your Name <you@example.com>
2022-02-13 11:17:59 +0530

    score

--- app/controllers/quiz_result_controller.rb
+++ app/controllers/quiz_result_controller.rb
@@ -7,5 +7,15 @@ class QuizResultController < ApplicationController
         @notification=Notification.find(params[:id])
         @notification.update(readed: 1)
         @quiz_results= @notification.quiz_results
+
+        @score=0
+        @quiz_results.each_with_index  do |quiz,index|            
+           
+            if  quiz.correct_ans == quiz.submited_ans
+                @score = @score+1
+            end
+        end
+
+        @score_data="Total student points and performance #{@score}/#{@quiz_results.count}"
     end
 end
